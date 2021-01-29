0477077b5cffd11049eab3fd3e7e524a6c169ec3
ChloeArd <chloe.ardoise@gmail.com>
2021-01-29 13:20:10 +0100

    Exercice 160

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
  * --> Attention à bien fermer votre fichier si vous utilisez fopen()
  */
 // TODO Votre code ici.
-
+$lorem = "Lorem ipsum dolor sit amet. Sed cumque mollitia aut blanditiis voluptatem ad possimus totam et dicta fugiat et blanditiis eaque.";
+$file = fopen('monFichier.txt', 'a+b');
+fwrite($file, $lorem."\n");
+fclose($file);
 
 /**
  * 3. Créez un tableau contenant au moins 10 chaînes de caractère au choix.
@@ -15,20 +18,29 @@
  * --> Attention: les chaînes de caractères doivent s'ajouter à la suite du contenu déjà existant, pas d'écrasement.
  */
 // TODO Votre code ici
+$table = ["bonjour", "comment", "ca", "va", "?", "Moi", "je", "vais", "bien"];
+$file = fopen('monFichier.txt', 'r+b');
+fwrite($file, $table."\n");
+fclose($file);
 
 
 /**
  * 5. Trouvez une solution pour afficher à l'aide d'un simple echo l'extension du fichier index.php
  */
 // TODO Votre code ici
-
+echo nl2br(file_get_contents("monFichier.txt"));
 
 
 /**
  * 6. Testez si le fichier 'toto' existe, sil n'existe pas, afficher un texte distant que ce fichier n'existe pas !
  */
 // TODO Votre code ici.
-
+if (file_exists("toto.txt")) {
+    echo "Le fichier toto.txt existe !";
+}
+else {
+    echo "Le fichier toto.txt n'existe pas !";
+}
 
 /**
  * Super bonus.
